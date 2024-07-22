1cdbd37b36995edb11c2f441555c7b4edf27407d
alxxdev <alxxdev@yandex.ru>
2024-07-22 19:14:33 +0300

    routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,6 @@ import (
 	"os"
 )
 
-// Define an application struct to hold the application-wide dependencies for the
-// web application. For now we'll only include fields for the two custom loggers, but
-// we'll add more to it as the build progresses.
 type application struct {
 	errorLog *log.Logger
 	innfoLog *log.Logger
@@ -23,26 +20,16 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Initialize a new instance of our application struct, containing the dependencies.
 	app := &application{
 		errorLog: errorLog,
 		innfoLog: infoLog,
 	}
 
-	// Swap the route declarations to use the application struct's methods as the handler functions.
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		// Call the new app.routes() method to get the servemux containing our routes.
+		Handler: app.routes(),
 	}
 	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
