9de0aff30ac947becc8ead7b6517f74c0e30e479
alxxdev <alxxdev@yandex.ru>
2024-08-01 23:48:11 +0300

    ch 5.55

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,9 +3,20 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/alxxdev/snippetbox/internal/models"
 )
+
+// humanDate function which returns a nicely formatted string representation of a time.Time object.
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable.
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
 
 // Include a Snippets field in the templateData struct.
 type templateData struct {
@@ -21,22 +32,22 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
-		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
-		// Call ParseGlob() *on this template set* to add any partials.
 		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
 			return nil, err
 		}
-		// Call ParseFiles() *on this template set* to add the page template.
 		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
-		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
 	return cache, nil
