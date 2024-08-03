8203fc339ab1b1a5a3d1a636fa09bfbeb0b31c66
alxxdev <alxxdev@yandex.ru>
2024-08-03 10:12:37 +0300

    ch 6-4 panic recovery

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,12 +4,14 @@ import "net/http"
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	// Wrap the existing chain with the logRequest middleware.
-	return app.logRequest(secureHeaders(mux))
+	// Wrap the existing chain with the recoverPanic middleware.
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
